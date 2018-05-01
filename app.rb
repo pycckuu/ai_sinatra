4d3ebe911639bff37b72b57f5228f5be93b4d183
pycckuu <is.markelov@gmail.com>
2018-05-01 19:30:01 +0200

    updated models and results

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
+load File.dirname(__FILE__) + '/model_results.rb'
 
 get '/' do
+   @model_res = @@models.sort_by { |k| k[:score] }.reverse
   erb :index
+end
+
+get '/plots' do
+  erb :plots
+end
+
+get '/steps_taken' do
+  redirect '/steps_taken.html'
 end
